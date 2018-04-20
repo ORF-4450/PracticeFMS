880c5cedcda5c99e8d1b1b8982efb7f9fde93b08
MoSadie <sean@cushmantech.net>
2018-04-20 10:26:57 -0700

    More work done.
    Not finished with Packet Creation.

--- PFMS/Program.cs
+++ PFMS/Program.cs
@@ -33,6 +33,9 @@ namespace PFMS
         static DriverStation blue2;
         static DriverStation blue3;
 
+        static enum GamePhase { PREMATCH, AUTO, PAUSE, TELEOP, POSTMATCH };
+        static GamePhase currentGamePhase = GamePhase.PREMATCH;
+
         static string redGameString;
         static string blueGameString;
 
@@ -211,6 +214,7 @@ namespace PFMS
         bool isRobotRadioConnected = false;
         bool isRoboRioConnected = false;
         AllianceStation allianceStation;
+        int packetCount = 0;
 
         ThreadStart pingThreadRef;
         Thread pingThread;
@@ -222,15 +226,65 @@ namespace PFMS
         Thread sendDataThread;
 
         UdpClient udpClient;
-        TcpClient tcpClient;
+        TcpListener tcpListener;
 
         public bool isRedAlliance() { return (allianceStation == AllianceStation.RED1 || allianceStation == AllianceStation.RED2 || allianceStation == AllianceStation.RED3); }
+
+        byte[] generateDriverStationControlPacket()
+        {
+            byte[] packet = new byte[22];
+
+            //Packet defination from 254's Cheesy Arena.
+            //Packet Count
+            packet[0] = (byte) ((packetCount >> 8) & 0xff);
+            packet[1] = (byte) (packetCount & 0xff);
+
+            //Version
+            packet[2] = 0;
+
+            //Robot Status
+            packet[3] = 0;
+            if (MainClass.currentGameState == MainClass.GameState.AUTO) { packet[3] |= 0x02; }
+            else if (MainClass.currentGameState == MainClass.GameState.TELEOP) { packet[3] |= 0x04; }
+
+            //Unused
+            packet[4] = 0;
+
+            //Alliance Station
+            packet[5] = (int)allianceStation;
+
+            //Match Type
+            packet[6] = 1; //Practice
+
+            //Match Number
+            packet[7] = 0;
+            packet[8] = 1;
+
+            //Repeat Number
+            packet[9] = 1;
+
+            //Current Time
+            DateTime currentTime = DateTime.now;
+            int nanoseconds = (currentTime.Ticks % TimeSpan.ticksPerMillasecond % 10) * 100;
+            packet[10] = (byte)(((nanoseconds / 1000) >> 24) & 0xff);
+            packet[11] = (byte)(((nanoseconds / 1000) >> 16) & 0xff);
+            packet[12] = (byte)(((nanoseconds / 1000) >> 8) & 0xff);
+            packet[13] = (byte)((nanoseconds / 1000) & 0xff);
+            packet[14] = (byte)currentTime.Second;
+            packet[15] = (byte)currentTime.Minute;
+            packet[16] = (byte)currentTime.Hour;
+            packet[17] = (byte)currentTime.Day;
+            packet[18] = (byte)currentTime.Month;
+            packet[19] = (byte)(currentTime.Year - 1900);
+
+            //TODO Match Time Remaining [20 and 21]
+        }
 
         public void robotPingThread()
         {
             Ping ping = new Ping();
             int timeout = 5;
-            while (true)
+            while (!closed)
             {
                 //Ping Robot Radio
                 PingReply result = ping.Send(radioIp, timeout);
@@ -250,13 +304,61 @@ namespace PFMS
 
         public void recieveStatusThread()
         {
-            byte[] buffer = new byte[4096];
             while (!closed)
             {
+                TcpClient client = tcpListener.AcceptTcpClient();
+                byte[] buffer = new byte[4096];
 
+
+                i = client.GetStream().Read(bytes, 0, bytes.Length);
+
+                while (i != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine(String.Format("Received: {0}", data));
+
+                    data = data.ToUpper();
+
+                    //Insert fms response here.
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine(String.Format("Sent: {0}", data));
+
+                    i = client.GetStream().Read(bytes, 0, bytes.Length);
+                }
             }
         }
     }
 
     enum AllianceStation { RED1, RED2, RED3, BLUE1, BLUE2, BLUE3 }
+
+    class DriverStationStatusPacket
+    {
+        static enum PacketType { KEEP_ALIVE, ROBOT_CONTROL, BAD_PACKET };
+
+        static DriverStationStatusPacket decodeDriverStationStatusPacket(byte[] data)
+        {
+            DriverStationStatusPacket packet = new DriverStationStatusPacket();
+			if ((int) data[2] == 28) {
+                packet.packetType = PacketType.KEEP_ALIVE;
+                return packet;
+			} else if ((int) data[2] != 22) {
+                packet.packetType = PacketType.BAD_PACKET;
+				return packet;
+			}
+
+            packet.packetType = PacketType. ROBOT_CONTROL;
+
+            return packet;
+        }
+
+        public DriverStationStatusPacket()
+        {
+
+        }
+		
+		public PacketType packetType;
+		
+    }
 }
